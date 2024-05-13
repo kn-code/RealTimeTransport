0eb82842b0bc8d659677fc8fcd15284580d4450c
kn-code <83916219+kn-code@users.noreply.github.com>
2024-05-13 05:50:36 +0200

    Add plot to example

--- examples/transient_anderson_dot.cpp
+++ examples/transient_anderson_dot.cpp
@@ -1,11 +1,19 @@
 // clang-format off
 
+#include <fstream>
+
+#include <SciCore/Serialization.h>
+
 #include <RealTimeTransport/Models/AndersonDot.h>
 #include <RealTimeTransport/RenormalizedPT/CurrentKernel.h>
 #include <RealTimeTransport/RenormalizedPT/MemoryKernel.h>
 
+#include "helper.h"
+
 using namespace RealTimeTransport;
 using namespace SciCore;
+
+void plot(const std::string& jsonDataFile, const std::string& pythonPlotFile);
 
 int main()
 {
@@ -39,7 +47,7 @@ int main()
     nUp.diagonal() << 0, 1, 0, 1;
 
     // Compute occupations Tr{nUp*rho(t)}
-    auto t = RealVector::LinSpaced(100, 0, tMax);
+    RealVector t = RealVector::LinSpaced(100, 0, tMax);
     RealVector n(t.size());
     for (int i = 0; i < t.size(); ++i)
         n[i] = (Pi(t[i], rho0) * nUp).trace().real();
@@ -57,5 +65,57 @@ int main()
     // Print
     std::cout << "I = " << I_t.transpose() << "\n";
 
+    // Save data as .json file
+    std::string jsonDataFile = "data-transient-anderson-dot.json";
+    {
+        std::ofstream os(jsonDataFile);
+        cereal::JSONOutputArchive archive(os);
+        archive(CEREAL_NVP(epsilon), CEREAL_NVP(B), CEREAL_NVP(U), CEREAL_NVP(T), CEREAL_NVP(mu), CEREAL_NVP(Gamma),
+                CEREAL_NVP(tMax), CEREAL_NVP(errGoal), CEREAL_NVP(t), CEREAL_NVP(n), CEREAL_NVP(I_t));
+    }
+
+    // Generate python script for plotting
+    std::string pythonPlotFile = "plot-transient-anderson-dot.py";
+    plot(jsonDataFile, pythonPlotFile);
+    std::cout << "Created python file for plotting: " << pythonPlotFile << "\n";
     return 0;
+
+    return 0;
+}
+
+void plot(const std::string& jsonDataFile, const std::string& pythonPlotFile)
+{
+    std::string pythonScript =
+
+R"(
+import matplotlib.pyplot as plt
+import numpy as np
+import json
+
+with open('{jsonDataFile}', 'r') as f:
+    data = json.load(f)
+
+t = np.array(data['t'])
+n = np.array(data['n'])
+I = np.array(data['I_t'])
+
+plt.rcParams.update({
+    'text.usetex': True,
+    'font.family': 'serif',
+    'font.size': 12
+})
+
+plt.plot(t, n, label=r'$n(t)$')
+plt.plot(t, I, label=r'$I(t)$')
+plt.legend()
+plt.xlabel(r'$t \Gamma$')
+plt.savefig('out.png', dpi=600, bbox_inches='tight')
+plt.show()
+)";
+
+    replaceTag(pythonScript, "{jsonDataFile}", jsonDataFile);
+
+    std::ofstream out(pythonPlotFile);
+    out << pythonScript;
+    out.close();
 }
